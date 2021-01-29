c4df184e8ee11cf2ed830b3ca372b331bcd94276
Konstantinos <ga45buf@mytum.de>
2021-01-29 09:51:31 +0100

    Safety push

--- geo_downstream.R
+++ geo_downstream.R
@@ -3,4 +3,9 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 
 BiocManager::install("GOSim")
 
+ines_pathways <- readRDS("use_case_data/geo_data/comparison/selected_ines_pathways.rds")
+
+example1 <- ines_pathways$`K-3-L1-10`
+#Obtain the GO terms and their description for a list of genes.
+getGOInfo(geneIDs = example1@nodes,)
 
