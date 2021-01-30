1fb9adb8285b9563bf0fd7e710c3006612869f52
Konstantinos <ga45buf@mytum.de>
2021-01-30 01:43:09 +0100

    SAFEETY PUSH

--- testing-topGO.R
+++ testing-topGO.R
@@ -4,11 +4,26 @@ indicator_matrix <- readRDS("use_case_data/geo_data/data/indicator_matrix_lfc1_p
 # Step 2: Create gene-to-GO mapping annotation ####
 gene_universe <- indicator_matrix$hgnc_symbol
 GOs <- translate(gene_universe , from = org.Hs.egALIAS2EG, to = org.Hs.egGO)
-# Step 3: 
-pickGO(GOs, category='MF')
+example <- pathways_of_interest$`K-1-L1-8-union`
 myIntersetingGenes <- example@nodes$node
 geneList <- factor(as.integer(gene_universe%in%myIntersetingGenes))
 names(geneList) <- gene_universe
 
-Godata <- new("topGOdata", ontology = "BP", allGenes = geneList,
-              annot = annFUN.gene2GO, gene2GO =  GOs)
+# Biological process ####
+GO_data <- new("topGOdata", ontology = "BP", allGenes = geneList, annot = annFUN.gene2GO, gene2GO = GOs)
+
+classic_fs <- runTest(GO_data, algorithm = "classic",statistic = "fisher")
+classic_ks <- runTest(GO_data, algorithm = "classic",statistic = "ks")
+#resultKS_elim <- runTest(GO_data, algorithm = "elim", statistic = "ks")
+#weight01_ks<- runTest(GO_data, algorithm = "weight01", statistic = "ks")
+table_bp <-  GenTable(GO_data, classicFisher = classic_fs, classicKS = classic_ks, orderBy = "classicFisher")
+  
+# Molecular function ####  
+GO_data <- new("topGOdata", ontology = "MF", allGenes = geneList, annot = annFUN.gene2GO, gene2GO =  GOs)
+classic_fs <- runTest(GO_data, algorithm = "classic",statistic = "fisher")
+classic_ks <- runTest(GO_data, algorithm = "classic",statistic = "ks")
+table_bp <-  GenTable(GO_data, classicFisher = classic_fs, classicKS = classic_ks, orderBy = "classicFisher")
+
+  # allRes <- GenTable(GO_data,
+  #                  classicFisher = resultFisher,  elimKS = resultKS_elim, weight01KS = weight01_ks,
+  #                  orderBy = "weight01KS", ranksOf = "classicKS", topNodes = 10)
