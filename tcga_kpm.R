5bcff5967b9c01afb8cb52d41d3d958a454fc22c
Konstantinos <ga45buf@mytum.de>
2021-01-31 14:12:28 +0100

    Safety push

--- tcga_kpm.R
+++ tcga_kpm.R
@@ -29,52 +29,52 @@ result <- new("Result", configurations = result_list, parameters = list(computed
 
 # Save result
 saveRDS(result, "use_case_data/gdc_data/kpm_results/ines_greedy_results_prostate_tumor_z_score_2.rds")
-# 
-# # Step 3: Run KPM INES and save results for z-score = 3 ####
-# # Since the datasets and biological network are very big we need to allocate some additional memory
-# options(java.parameters = "-Xmx64000m")
-# result_list <- list()
-# 
-# # Greedy INES run
-# for (l in c(150, 200, 250)) {
-#   for (k in c(2:20)) {
-#     kpm_options(
-#       execution = "Local",
-#       strategy = "INES",
-#       algorithm = "Greedy",
-#       l_min = l,
-#       k_min = k
-#     )
-#     result_list <- c(result_list,  kpm(graph = human_string_network, indicator_matrices = counts_matrix_z_3)@configurations)
-#   }
-# }
-# # Save pathways from several runs in a Result object
-#result <- new("Result", configurations = result_list, parameters = list(computed_pathways = kpm_options()$computed_pathways, strategy = kpm_options()$strategy))
-# 
-# # Save result
-# saveRDS(result, "use_case_data/gdc_data/kpm_results/ines_greedy_results_prostate_tumor_z_score_3.rds")
-# 
-# # Step 4: Run KPM INES and save results for z-score = 4 ####
-# # Since the datasets and biological network are very big we need to allocate some additional memory
-# options(java.parameters = "-Xmx64000m")
-# result_list <- list()
-# 
-# # Greedy INES run
-# for (l in c(150, 200, 250)) {
-#   for (k in c(2:20)) {
-#     kpm_options(
-#       execution = "Local",
-#       strategy = "INES",
-#       algorithm = "Greedy",
-#       l_min = l,
-#       k_min = k
-#     )
-#     result_list <- c(result_list,  kpm(graph = human_string_network, indicator_matrices = counts_matrix_z_4)@configurations)
-#   }
-# }
-# 
-# # Save pathways from several runs in a Result object
-# result <- new("Result", configurations = result_list, parameters = list(computed_pathways = kpm_options()$computed_pathways, strategy = kpm_options()$strategy))
-# 
-# # Save result
-# saveRDS(result, "use_case_data/gdc_data/kpm_results/ines_greedy_results_prostate_tumor_z_score_4.rds")
+
+# Step 3: Run KPM INES and save results for z-score = 3 ####
+# Since the datasets and biological network are very big we need to allocate some additional memory
+options(java.parameters = "-Xmx64000m")
+result_list <- list()
+
+# Greedy INES run
+for (l in c(150, 200, 250)) {
+  for (k in c(2:20)) {
+    kpm_options(
+      execution = "Local",
+      strategy = "INES",
+      algorithm = "Greedy",
+      l_min = l,
+      k_min = k
+    )
+    result_list <- c(result_list,  kpm(graph = human_string_network, indicator_matrices = counts_matrix_z_3)@configurations)
+  }
+}
+# Save pathways from several runs in a Result object
+result <- new("Result", configurations = result_list, parameters = list(computed_pathways = kpm_options()$computed_pathways, strategy = kpm_options()$strategy))
+
+# Save result
+saveRDS(result, "use_case_data/gdc_data/kpm_results/ines_greedy_results_prostate_tumor_z_score_3.rds")
+
+# Step 4: Run KPM INES and save results for z-score = 4 ####
+# Since the datasets and biological network are very big we need to allocate some additional memory
+options(java.parameters = "-Xmx64000m")
+result_list <- list()
+
+# Greedy INES run
+for (l in c(150, 200, 250)) {
+  for (k in c(2:20)) {
+    kpm_options(
+      execution = "Local",
+      strategy = "INES",
+      algorithm = "Greedy",
+      l_min = l,
+      k_min = k
+    )
+    result_list <- c(result_list,  kpm(graph = human_string_network, indicator_matrices = counts_matrix_z_4)@configurations)
+  }
+}
+
+# Save pathways from several runs in a Result object
+result <- new("Result", configurations = result_list, parameters = list(computed_pathways = kpm_options()$computed_pathways, strategy = kpm_options()$strategy))
+
+# Save result
+saveRDS(result, "use_case_data/gdc_data/kpm_results/ines_greedy_results_prostate_tumor_z_score_4.rds")
